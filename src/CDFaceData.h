f464a65e8c24f054d51643cf4ac94b246318cf55
Dmytrii Shchadei <gmetrofun@gmail.com>
2016-01-13 00:43:08 +0100

    Made public required properties of FaceModel

--- src/CDFaceData.h
+++ src/CDFaceData.h
@@ -20,80 +20,71 @@
 #include "CDMesh.h"
 #include "CDMeshMapper.h"
 
-
-class CDFaceDistortionUnit
-{
+class CDFaceDistortionUnit {
 public:
-	CDFaceDistortionUnit( const std::string& name = "" );
-	void setName( const std::string& n ) { name = n; }
-	std::string getName() const { return name; }
-	
-	void addVertexOffset( unsigned short vIdx, glm::vec3 offset );
-	
-	/// apply this shape unit to the target mesh with the given factor (usually 0..1)
-	void apply( float amount, CDMesh& target ) const;
-	
+    CDFaceDistortionUnit(const std::string& name = "");
+    void setName(const std::string& n) { name = n; }
+    std::string getName() const { return name; }
+
+    void addVertexOffset(unsigned short vIdx, glm::vec3 offset);
+
+    /// apply this shape unit to the target mesh with the given factor (usually 0..1)
+    void apply(float amount, CDMesh& target) const;
+
+    std::map<unsigned short, glm::vec3> offsets;
+
 private:
-	
-	std::string name;
-	std::map<unsigned short, glm::vec3> offsets;
-	
-	
+    std::string name;
 };
 
-
-class CDFaceData
-{
+class CDFaceData {
 public:
-	/// filename is in wfm format, see candide3
-	CDFaceData( const std::string &path );
-	
-	void clear();
+    /// filename is in wfm format, see candide3
+    CDFaceData(const std::string& path);
 
-	void calculateCompensatoryTranslateScale( const glm::vec3& centerPos, const glm::vec3& fitSize, glm::vec3 &tranlateOut, float &scaleOut );
-	void draw();
-	
-	std::vector<std::string> getAnimationUnitNames();
-	std::vector<std::string> getShapeUnitNames();
-	
-	float getAnimationUnitValue( const std::string& auName );
-	void setAnimationUnitValue( const std::string& auName, float value );
-	
-	float getShapeUnitValue( const std::string& suName );
-	void setShapeUnitValue( const std::string &suName, float value );
-	
-	
-	void deserialize( const picojson::value& source );
-	picojson::value serialize() const;
-	
-	CDMesh getDistortedMesh() const;
+    void clear();
 
-	const CDMesh& getControlMeshForMeanValueDeformation();
-	
-	sigc::signal<void> controlMeshChangedSignal;
-	
+    void calculateCompensatoryTranslateScale(const glm::vec3& centerPos, const glm::vec3& fitSize, glm::vec3& tranlateOut, float& scaleOut);
+    void draw();
+
+    std::vector<std::string> getAnimationUnitNames();
+    std::vector<std::string> getShapeUnitNames();
+
+    float getAnimationUnitValue(const std::string& auName);
+    void setAnimationUnitValue(const std::string& auName, float value);
+
+    float getShapeUnitValue(const std::string& suName);
+    void setShapeUnitValue(const std::string& suName, float value);
+
+    void deserialize(const picojson::value& source);
+    picojson::value serialize() const;
+
+    CDMesh getDistortedMesh() const;
+
+    const CDMesh& getControlMeshForMeanValueDeformation();
+
+    sigc::signal<void> controlMeshChangedSignal;
+
+    std::vector<CDFaceDistortionUnit> shapeUnits;
+    std::vector<CDFaceDistortionUnit> animationUnits;
+
 private:
-	
-	void load( const std::string& path );
-	
-	/// returns npos if not found
-	size_t getIndexOfShapeUnit( const std::string& name );
-	size_t getIndexOfAnimationUnit( const std::string& name );
+    void load(const std::string& path);
 
-	// data
-	CDMesh meshAtRest;
-	std::vector<CDFaceDistortionUnit> shapeUnits;
-	std::vector<CDFaceDistortionUnit> animationUnits;
-	
-	// settings for distortion
-	std::map<size_t,float> shapeUnitSettings;
-	std::map<size_t,float> animationUnitSettings;;
-	
-	CDMesh controlMeshForMeanValueDeformation;
-	CDMeshMapper controlMeshMapper;
-	
-	
+    /// returns npos if not found
+    size_t getIndexOfShapeUnit(const std::string& name);
+    size_t getIndexOfAnimationUnit(const std::string& name);
+
+    // data
+    CDMesh meshAtRest;
+
+    // settings for distortion
+    std::map<size_t, float> shapeUnitSettings;
+    std::map<size_t, float> animationUnitSettings;
+    ;
+
+    CDMesh controlMeshForMeanValueDeformation;
+    CDMeshMapper controlMeshMapper;
 };
-
 
 #endif /* defined(__candide__CandideFaceData__) */
